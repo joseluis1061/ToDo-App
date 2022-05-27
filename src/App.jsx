2bb1015270ff068a775a8fa78e1fc41d39ab9a4a
jose <joseluis836@hotmail.com>
2022-05-27 16:09:03 -0500

    Se agrego un hook de efecto, con el simulamos que el cambio en nuestra lista de tareas tiene un delete similar a hacer la petición a una Api o base de datos. Simulamos la respuesta de error y de estado cargando

--- src/App.jsx
+++ src/App.jsx
@@ -9,55 +9,32 @@ import { Counter } from './components/Counter'
 function useLocalStorage(itemName, initialValue){
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(true);
-  //Ahora debo crear el hook que producira los cambios de variable item
-  //al interior de mi app iniciandolos con el valor inicial que obtuve
-  //desde el local Storage
   const [item, setItem] = useState(initialValue);
 
 
   useEffect(()=>{
     setTimeout(()=>{
       try{
-        //Solicita la base de datos de local storage
         const localList = localStorage.getItem(itemName);
-        //Inicio la variable que contiene la info de tareas
         let listaStorage;
-        //Compruebo si obtengo datos, en caso de que no...
         if(!localList){
-          //Inicio un item con estado vacio de la base de datos localStorage
           localStorage.setItem(itemName, JSON.stringify(initialValue));
-          //Tambien inicio mi variable de App en el mismmo estado vacio
           listaStorage = initialValue;
         }else{
-          //Si obtengo datos lo transformo de Json a un tipo de dato
-          //que pueda procesar y consumir en mi APP
           listaStorage = JSON.parse(localList);
         }
-        //Si obtengo respuesta cambio el estado de mi variable
-        //App por el que obtuve desde mi localStorage
         setItem(listaStorage);
-        //Indico que ya no estoy cargando mi respuesta
         setLoading(false);
       } catch(error) {
-        //En caso de no poder conectarme al localStorage genero un error
         setError(true);
       }
-
     }, 1000);
-
   },[]);
-
-
-  //Este método lo usaremos para actualizar el local storage y el
-  //item de la APP mediante la función saveItem que recibe datos
-  //desde nuestra APP. En este caso llame la variable newItem
+  
   const saveItem = (newItem) => {
     try{
-      //El dato recibido lo convertimos a JSON
       const stringifiedItem = JSON.stringify(newItem);
-      //Y con el actualizo el local Storage
       localStorage.setItem(itemName, stringifiedItem);
-      //Con el dato recibido actualizo el estado de la variable en la APP
       setItem(newItem);
     }catch(error){
       setError(error);
@@ -65,8 +42,6 @@ function useLocalStorage(itemName, initialValue){
 
   };
 
-  //El hoock debe retornar la función de cambio de valor
-  //y el nuevo valor que vamos a usar dentro de un array.
   return {
     item,
     saveItem,
@@ -77,7 +52,6 @@ function useLocalStorage(itemName, initialValue){
 
 
 function App() {
-
   const {
     item:listaTareas,
     saveItem:setListaTareas,
@@ -85,9 +59,6 @@ function App() {
     error,
   } = useLocalStorage('listaTareas_V1', []);
 
-  
-  console.log(`Mi lista ${listaTareas}`)
-  // const [listaTareas, setListaTareas] = useState(listaStorage);
   const [serchText, setSerchText] = useState('');
 
   //Conteo completadas y totales
